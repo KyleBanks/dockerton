b21e4d6101ee84a8003257936d92b7f61120a068
KyleBanks <kyle@nthgensoftware.com>
2015-11-18 12:42:52 -0500

    Updating whalesay example to generate dockerfile and build image

--- examples/whalesay.js
+++ examples/whalesay.js
@@ -1,7 +1,9 @@
 /**
  * whalesay.js
  *
- * Most basic example using the `whalesay` tutorial from docker.com
+ * A very basic example using the `whalesay` tutorial from docker.com.
+ *
+ * See https://docs.docker.com/linux/step_four/
  *
  * Created by kylewbanks on 15-11-17.
  */
@@ -16,4 +18,26 @@ var Dockerton = require('../index');
  * @private
  */
 var dockerton = new Dockerton()
-    .from('docker/whalesay', 'latest');
+    .from('docker/whalesay', 'latest')
+    .run('apt-get -y update && apt-get install -y fortunes')
+    .cmd('/usr/games/fortune -a | cowsay');
+
+dockerton.dockerfile()
+    .then(function(contents) {
+        console.log('---------------------');
+        console.log('Generated Dockerfile:');
+        console.log(contents);
+        console.log('---------------------');
+
+        return dockerton.buildImage();
+    })
+    .then(function() {
+        console.log('---------------------');
+        console.log('Image Built');
+        console.log('---------------------');
+
+        // TODO
+    })
+    .catch(function(err) {
+        throw err;
+    });
